5af96a3d94d690fc2b684a68372a16826bc8163c
ryzmdn <ryzmdn@gmail.com>
2026-02-14 01:11:19 +0700

    feat: remove old components and styles, add Home page and global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import Home from "./page.tsx";
+import "@/assets/css/globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <Home />
     </ThemeProvider>
   </StrictMode>,
 );
